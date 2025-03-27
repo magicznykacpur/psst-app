2989a531f39ba04548a527f8c34304543a076f22
magicznykacpur <kafejek@gmail.com>
2025-03-27 10:02:37 +0100

    use electron-vite starter

--- src/renderer.ts
+++
@@ -1,5 +0,0 @@
-import { electronApi } from "./preload"
-
-const information = document.getElementById("info")
-information.innerText = "HABIDI"
-// information.innerText = `This app is using chrome (v${electronApi.versions.chrome()})`
