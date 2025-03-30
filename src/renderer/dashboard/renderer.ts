7215f0202c4b95cb4fae30c7bf4c9f8ced2eba4e
magicznykacpur <kafejek@gmail.com>
2025-03-30 14:07:39 +0200

    display messages from chat, load messages on chat load

--- src/renderer/dashboard/renderer.ts
+++ src/renderer/dashboard/renderer.ts
@@ -6,6 +6,7 @@ const initDashboardRenderer = () => {
     await getUsersChats();
     initHeaderDropdownListener();
     initSignoutUser();
+    initMessageInputHandler();
   });
 };
 
@@ -50,13 +51,28 @@ const getUser = async () => {
   }
 };
 
-type UsersChats = {
+type ChatUser = { id: string; username: string };
+
+type Chat = {
   id: string;
   created_at: string;
   updated_at: string;
-  receiver: { id: string; username: string };
-}[];
-let usersChats: UsersChats | [] = [];
+  sender: ChatUser;
+  receiver: ChatUser;
+};
+
+type Message = {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  body: string;
+  sender_id: string;
+  receiver_id: string;
+};
+
+let usersChats: Chat[] | [] = [];
+let currentChat: Chat | null = null;
+let currentMessages: Message[] | [] = [];
 
 const getUsersChats = async () => {
   try {
@@ -67,22 +83,29 @@ const getUsersChats = async () => {
       },
     });
 
-    const data = await response.json();
-    usersChats = data
+    const data: Chat[] = await response.json();
+    usersChats = data;
+    currentChat = data[0];
 
-    populateChats(usersChats)
+    populateChats(usersChats);
+
+    const messages = await getMessagesForChat(currentChat.id);
+    currentMessages = messages;
+
+    populateMessages(currentMessages);
   } catch (error) {
     console.log(error);
     promptToast("couldn't retrieve users chats");
   }
 };
 
-const populateChats = (data: UsersChats) => {
+const populateChats = (data: Chat[]) => {
   if (data.length > 0) {
     const chats = document.getElementById("chats") as HTMLDivElement;
 
     data.forEach((chat) => {
       const chatItem = document.createElement("div");
+      initChatItemEventListener(chatItem, chat);
       chatItem.classList.add(
         "chat-item",
         "d-flex",
@@ -91,15 +114,105 @@ const populateChats = (data: UsersChats) => {
       );
 
       const chatWith = document.createElement("div");
-
+      const chatName =
+        chat.receiver.id === user?.id
+          ? chat.sender.username
+          : chat.receiver.username;
       chatWith.classList.add("chat-with");
-      chatWith.innerText = chat.receiver.username;
+      chatWith.innerText = chatName;
 
       chatItem.appendChild(chatWith);
-
       chats.appendChild(chatItem);
     });
   }
+};
+
+const initChatItemEventListener = (chatItem: HTMLDivElement, chat: Chat) => {
+  const messages = document.getElementById("messages") as HTMLDivElement;
+  messages.innerHTML = "";
+
+  chatItem.addEventListener("click", async () => {
+    currentChat = chat;
+
+    try {
+      const messages = await getMessagesForChat(chat.id);
+      currentMessages = messages;
+      populateMessages(currentMessages);
+    } catch (error) {
+      console.error(error);
+      promptToast("couldn't fetch messages for this chat");
+    }
+  });
+};
+
+const getMessagesForChat = async (chatId: string) => {
+  const response = await fetch(`${window.api_url}/messages/${chatId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${userToken}`,
+    },
+  });
+
+  const data: Message[] = await response.json();
+  return data;
+};
+
+const populateMessages = (messages: Message[]) => {
+  const messagesEl = document.getElementById("messages") as HTMLDivElement;
+  messagesEl.innerHTML = "";
+
+  if (messages.length === 0) {
+    const noMessages = document.createElement("div")
+    noMessages.classList.add("no-messages", "mt-5")
+    noMessages.innerText = "this chat has no messages yet"
+
+    messagesEl.appendChild(noMessages)
+    return
+  }
+
+  messages.forEach((message) => {
+    const messageEl = document.createElement("div");
+    messageEl.classList.add(
+      message.sender_id === user?.id ? "my-message" : "receiver-message"
+    );
+    messageEl.innerText = message.body;
+
+    messagesEl.appendChild(messageEl);
+    messagesEl.scrollTop = messagesEl.scrollHeight
+  });
+};
+
+const initMessageInputHandler = () => {
+  const messageInput = document.getElementById(
+    "message-input"
+  ) as HTMLTextAreaElement;
+
+  messageInput.addEventListener("keypress", async (event) => {
+    if (event.key === "Enter") {
+      try {
+        await fetch(`${window.api_url}/messages`, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+          body: JSON.stringify({
+            chat_id: currentChat?.id,
+            sender_id: user?.id,
+            receiver_id:
+              currentChat?.sender.id === user?.id
+                ? currentChat?.receiver.id
+                : currentChat?.sender.id,
+            body: messageInput.value,
+          }),
+        });
+
+        messageInput.value = "";
+      } catch (error) {
+        console.log(error);
+        promptToast("couldn't send message");
+      }
+    }
+  });
 };
 
 const initSignoutUser = () => {
